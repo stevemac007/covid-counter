3c209ab2924b07b0b8e66b3a10de23350dc66370
Steve Mactaggart <steve@whitesquaresoft.com>
2020-05-30 20:43:59 +1000

    First simple version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = (val) => {
+    setCount(count + val);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <main>
+          <div className="location">
+            Cevo BusyPlace
+          </div>
+          <div className="current-count">
+            <div className={(count < 50) ? "value safe" : (count > 90) ? "value danger" : "value warn"}>{count}</div>
+          </div>
+          <div className="add-buttons">
+            <button onClick={e => incrementCount(1)}>+1</button>
+            <button onClick={e => incrementCount(2)}>+2</button>
+            <button onClick={e => incrementCount(5)}>+5</button>
+          </div>
+          <div className="remove-buttons">
+            <button onClick={e => incrementCount(-1)}>-1</button>
+            <button onClick={e => incrementCount(-2)}>-2</button>
+            <button onClick={e => incrementCount(-5)}>-5</button>
+          </div>
+        </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { useRef, useState } from 'react';
 
 ReactDOM.render(
   <React.StrictMode>
